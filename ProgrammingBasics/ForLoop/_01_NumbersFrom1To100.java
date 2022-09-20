bb5e64bb982a2edd9da7bb4e826102f7ea9e2393
Radi <radi03825@gmail.com>
2022-09-20 11:10:44 +0300

    Commit

--- ProgrammingBasics/ForLoop/_01_NumbersFrom1To100.java
+++ ProgrammingBasics/ForLoop/_01_NumbersFrom1To100.java
@@ -1,10 +1,7 @@
-public class Nums_02 {
+public class _01_NumbersFrom1To100 {
     public static void main(String[] args) {
-
-        for (int i = 1; i <= 10; i++){
+        for(int i = 1; i <= 100; i++){
             System.out.println(i);
         }
-
-
     }
 }
